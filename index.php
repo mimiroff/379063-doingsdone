f36631c923ff73f38144ea59a1d4fab439face21
Ilia Barashkov <ibarashkov@list.ru>
2018-04-25 22:36:50 +0300

    Task 1.2

--- index.php
+++ index.php
@@ -97,7 +97,8 @@ $show_complete_tasks = rand(0, 1);
 
                     <label class="checkbox">
                         <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox">
+                        <?php $checked = ($show_complete_tasks == 1) ? "checked" : ""; ?>
+                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?=$checked?>>
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -118,20 +119,20 @@ $show_complete_tasks = rand(0, 1);
                         <td class="task__date"></td>
                     </tr>
                     <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
+                    <?php if($show_complete_tasks == 1): ?>
                     <tr class="tasks__item task task--completed">
                         <td class="task__select">
                             <label class="checkbox task__checkbox">
-                                <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" checked>
-                                <span class="checkbox__text">Сделать главную страницу Дела в порядке</span>
+                                <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                                <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
                             </label>
                         </td>
+                        <td class="task__date">10.04.2017</td>
 
-                        <td class="task__file">
-                            <a class="download-link" href="#">Home.psd</a>
+                        <td class="task__controls">
                         </td>
-
-                        <td class="task__date"></td>
                     </tr>
+                    <?php endif; ?>
                 </table>
             </main>
         </div>
